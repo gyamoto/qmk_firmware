691ef01091f5c5a787fd9e8311e38929cc5243d6
gyamoto <farundorlnightray@gmail.com>
2019-03-24 22:18:42 +0900

    Replace up and down

--- keyboards/ergo42/keymaps/gyamoto/keymap.c
+++ keyboards/ergo42/keymaps/gyamoto/keymap.c
@@ -21,14 +21,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
    * | LSft |   Z  |   X  |   C  |   V  |  B   |  [   |   |   ]  |   N  |   M  |   ,  |   .  |   /  | RSft |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * | ESC  | LCtrl| LAlt | SYMB | META | LGUI |Space |   | Enter| RGUI | SYMB | Left | Down |  Up  | Right|
+   * | ESC  | LCtrl| LAlt | SYMB | META | LGUI |Space |   | Enter| RGUI | SYMB | Left |  Up  | Down | Right|
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [BASE] = LAYOUT( \
     KC_TAB,  KC_Q,    KC_W,     KC_E,     KC_R,     KC_T,    KC_GRV,     KC_BSLS, KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC, \
     KC_LCTL, KC_A,    KC_S,     KC_D,     KC_F,     KC_G,    KC_MINS,    KC_EQL,  KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,     KC_C,     KC_V,     KC_B,    KC_LBRC,    KC_RBRC, KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_ESC,  KC_LCTL, KC_LALT,  MO(SYMB), MO(META), KC_LGUI, KC_SPC,     KC_ENT,  KC_RGUI, MO(SYMB), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+    KC_ESC,  KC_LCTL, KC_LALT,  MO(SYMB), MO(META), KC_LGUI, KC_SPC,     KC_ENT,  KC_RGUI, MO(SYMB), KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT \
   ),
 
   /* META
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
    * | LSft |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 | RSft |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * | ESC  | LCtrl| LAlt | SYMB | META | LGUI |Space |   | Enter| RGUI | SYMB | Left | Down |  Up  | Right|
+   * | ESC  | LCtrl| LAlt | SYMB | META | LGUI |Space |   | Enter| RGUI | SYMB | Left |  Up  | Down | Right|
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [META] = LAYOUT( \
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
    * | LSft |      |      |      |      |      |      |   |      |      |      |      |      |      | RSft |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * | ESC  | LCtrl| LAlt | SYMB | META | LGUI |Space |   | Enter| RGUI | SYMB | Left | Down |  Up  | Right|
+   * | ESC  | LCtrl| LAlt | SYMB | META | LGUI |Space |   | Enter| RGUI | SYMB | Left |  Up  | Down | Right|
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [SYMB] = LAYOUT( \
